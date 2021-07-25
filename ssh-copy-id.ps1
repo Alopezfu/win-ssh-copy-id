c1cbc5ac2ce5d84d71a7ee6c5f44c7e2c4def8b1
Alejandro López <alejandrolopezfuentes789@gmail.com>
2021-07-25 08:30:35 +0200

    v1.0

--- ssh-copy-id.ps1
+++ ssh-copy-id.ps1
@@ -1,43 +1,105 @@
-$id_rsa = "C:\Users\$env:UserName\.ssh\id_rsa.pub"
-if (Test-Path $id_rsa)
-{
-    Clear-Host
-    $server = Read-Host -Prompt 'Input your server  name or Ip'
-    $user = Read-Host -Prompt 'Username ssh'
-    $port = Read-Host -Prompt 'Port'
-    scp -P $port $id_rsa  $user@$server':'/home/$user/ 
-    Write-Host ":a:"
-    ssh $user@$server -p $port "if [ ! -f /home/$user/.ssh/authorized_keys ]; then mkdir /home/$user/.ssh ; touch /home/$user/.ssh/authorized_keys ; fi"
-    if ($?){
+# Author: Alejandro López - 2021
+# GitHub: https://github.com/Alopezfu/win-ssh-copy-id
 
-        ssh $user@$server -p $port "cat /home/$user/id_rsa.pub >> /home/$user/.ssh/authorized_keys && rm -f /home/$user/id_rsa.pub && chmod 700 /home/$user/.ssh/ && chmod 600 /home/$user/.ssh/authorized_keys"
-        if($?){
+function spawnBanner {
 
-            Clear-Host
-            Write-Host "Process completed successfully!"
-            Write-Host "Now you can connect with - ssh $user@$server -p $port"
-        }else{
+    Clear-Host;
+    Write-Host -ForegroundColor Green "    __          ___          _____     _        _____                    _____    _ 
+    \ \        / (_)        / ____|   | |      / ____|                  |_   _|  | |
+     \ \  /\  / / _ _ __   | (___  ___| |__   | |     ___  _ __  _   _    | |  __| |
+      \ \/  \/ / | | '_ \   \___ \/ __| '_ \  | |    / _ \| '_ \| | | |   | | / _` |
+       \  /\  /  | | | | |  ____) \__ \ | | | | |___| (_) | |_) | |_| |  _| || (_| |
+        \/  \/   |_|_| |_| |_____/|___/_| |_|  \_____\___/| .__/ \__, | |_____\__,_|
+                                                          | |     __/ |             
+               Author: github.com/alopezfu                |_|    |___/   
+                               
+";
+}
 
-            Write-Host "Error"
+function help {
+    spawnBanner;
+    Write-Host -ForegroundColor DarkCyan "Usage: .\ssh-copy-id.ps1 [OPTION]
+
+Options:
+  -h, --host    Set remote hostname or IP.
+  -u, --user    Set remote username.
+  -p, --port    Set remote port with '' (If not set default value is 22).
+  --help        Show help.
+  
+Examples:
+  .\ssh-copy-id.ps1 --host srv0.mydoamin.com --user admin --port '25'
+  .\ssh-copy-id.ps1 -h srv0.myanotherdoamin.com -u pepe 
+  "
+    
+}
+
+function checkInput($options) {
+
+    if (($options[0] -and $options[0] -eq "-h" -or $options[0] -eq "--host" -and $options[1]::IsNullOrEmpty) -and
+        ($options[2] -and $options[2] -eq "-u" -or $options[2] -eq "--user" -and $options[3]::IsNullOrEmpty)) {
+
+        $hostname = $options[1];
+        $username = $options[3];
+
+        if ($options[4] -and $options[4] -eq "-p" -or $options[4] -eq "--port" -and $options[5]::IsNullOrEmpty) {
+
+            $port = $options[5];
         }
-    }else{
+        else {
 
-        Write-Host "Error"
+            $port = "22";
+        }
+        
+        
+    }
+    else {
+
+        help;
+        exit 1;
     }
 
-}else{
+    $out = @($hostname, $username, $port);
 
-    Clear-Host
-    Write-Host "You need create a public key.";
-    $q = Read-Host -Prompt "Generate now? (yes/no)"
-    if ($q -eq "yes"){
-
-        mkdir C:\Users\$env:UserName\.ssh\ 2>&1> $null
-        ssh-keygen -f "C:\Users\$env:UserName\.ssh\id_rsa"
-        .\ssh-copy-id.ps1
-    }else{
-
-        exit 0
-    }
+    return $out;
 
 }
+
+function publicKey {
+    
+    spawnBanner;
+    Write-Host -NoNewline -ForegroundColor DarkCyan "You need create a public key. Generate now? (yes/no): "
+    $q = Read-Host;
+    spawnBanner;
+    Write-Host -ForegroundColor DarkCyan "[*] Generate public key..."
+    if ($q -eq "yes") {
+
+        mkdir $env:USERPROFILE\.ssh\ 2>&1> $null
+        ssh-keygen -f "$env:USERPROFILE\.ssh\id_rsa"
+    }
+}
+
+function configHost($data) {
+    
+    spawnBanner;
+    Write-Host -ForegroundColor DarkCyan "[*] Config remote ssh..."
+    $hostname = $data[0];
+    $username = $data[1];
+    $port = $data[2];
+    scp -o StrictHostKeyChecking=no -P "$port" $env:USERPROFILE\.ssh\id_rsa.pub $username'@'$hostname':/home/'$username
+    
+    spawnBanner;
+    Write-Host -ForegroundColor DarkCyan "[*] Config remote ssh..."
+    ssh $username@$hostname -p $port "if [ ! -f /home/$username/.ssh/authorized_keys ]; then mkdir /home/$username/.ssh ; touch /home/$username/.ssh/authorized_keys ; fi ; cat /home/$username/id_rsa.pub >> /home/$username/.ssh/authorized_keys && rm -f /home/$username/id_rsa.pub && chmod 700 /home/$username/.ssh/ && chmod 600 /home/$username/.ssh/authorized_keys"
+}
+
+function main($data) {
+    
+    if (-not(Test-Path "$env:USERPROFILE\.ssh\id_rsa.pub")) {
+        
+        publicKey;
+    }
+
+    configHost(checkInput($data));
+}
+
+main($args);
