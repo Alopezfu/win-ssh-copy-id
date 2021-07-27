57db3cff4eea2c9f6443061d7d11f33e9d4035d1
Alejandro López <alejandrolopezfuentes789@gmail.com>
2021-07-27 10:25:02 +0200

    Fix keygen

--- ssh-copy-id.ps1
+++ ssh-copy-id.ps1
@@ -4,15 +4,15 @@
 function spawnBanner {
 
     Clear-Host;
-    Write-Host -ForegroundColor Green "    __          ___          _____     _        _____                    _____    _ 
+    Write-Host -ForegroundColor Green "    __          ___          _____     _        _____                    _____    _
     \ \        / (_)        / ____|   | |      / ____|                  |_   _|  | |
      \ \  /\  / / _ _ __   | (___  ___| |__   | |     ___  _ __  _   _    | |  __| |
       \ \/  \/ / | | '_ \   \___ \/ __| '_ \  | |    / _ \| '_ \| | | |   | | / _` |
        \  /\  /  | | | | |  ____) \__ \ | | | | |___| (_) | |_) | |_| |  _| || (_| |
         \/  \/   |_|_| |_| |_____/|___/_| |_|  \_____\___/| .__/ \__, | |_____\__,_|
-                                                          | |     __/ |             
-               Author: github.com/alopezfu                |_|    |___/   
-                               
+                                                          | |     __/ |
+               Author: github.com/alopezfu                |_|    |___/
+
 ";
 }
 
@@ -25,12 +25,12 @@ Options:
   -u, --user    Set remote username.
   -p, --port    Set remote port with '' (If not set default value is 22).
   --help        Show help.
-  
+
 Examples:
   .\ssh-copy-id.ps1 --host srv0.mydoamin.com --user admin --port '25'
-  .\ssh-copy-id.ps1 -h srv0.myanotherdoamin.com -u pepe 
+  .\ssh-copy-id.ps1 -h srv0.myanotherdoamin.com -u pepe
   "
-    
+
 }
 
 function checkInput($options) {
@@ -49,8 +49,6 @@ function checkInput($options) {
 
             $port = "22";
         }
-        
-        
     }
     else {
 
@@ -65,23 +63,26 @@ function checkInput($options) {
 }
 
 function publicKey {
-    
-    spawnBanner;
-    Write-Host -NoNewline -ForegroundColor Yellow "[*] "
-    Write-Host -NoNewline -ForegroundColor DarkCyan "You need create a public key. Generate now? (yes/no): "
-    $q = Read-Host;
-    spawnBanner;
-    Write-Host -NoNewline -ForegroundColor Yellow "[*] "
-    Write-Host -ForegroundColor DarkCyan "Generate public key..."
-    if ($q -eq "yes") {
 
-        mkdir $env:USERPROFILE\.ssh\ 2>&1> $null
-        ssh-keygen -f "$env:USERPROFILE\.ssh\id_rsa"
+    spawnBanner;
+    if (!Test-Path $env:USERPROFILE\.ssh\id_rsa) {
+
+        Write-Host -NoNewline -ForegroundColor Yellow "[*] "
+        Write-Host -NoNewline -ForegroundColor DarkCyan "You need create a public key. Generate now? (yes/no): "
+        $q = Read-Host;
+        spawnBanner;
+        Write-Host -NoNewline -ForegroundColor Yellow "[*] "
+        Write-Host -ForegroundColor DarkCyan "Generate public key..."
+        if ($q -eq "yes") {
+
+            mkdir $env:USERPROFILE\.ssh\ 2>&1> $null
+            ssh-keygen -f "$env:USERPROFILE\.ssh\id_rsa"
+        }
     }
 }
 
 function configHost($data) {
-    
+
     spawnBanner;
     Write-Host -NoNewline -ForegroundColor Yellow "[*] "
     Write-Host -ForegroundColor DarkCyan "Config remote ssh..."
@@ -89,20 +90,19 @@ function configHost($data) {
     $username = $data[1];
     $port = $data[2];
     scp -o StrictHostKeyChecking=no -P $port $env:USERPROFILE\.ssh\id_rsa.pub $username'@'$hostname':/home/'$username
-    
+
     spawnBanner;
     Write-Host -NoNewline -ForegroundColor Yellow "[*] "
     Write-Host -ForegroundColor DarkCyan "Config remote ssh..."
     ssh $username@$hostname -p $port "if [ ! -f /home/$username/.ssh/authorized_keys ]; then mkdir /home/$username/.ssh ; touch /home/$username/.ssh/authorized_keys ; fi ; cat /home/$username/id_rsa.pub >> /home/$username/.ssh/authorized_keys && rm -f /home/$username/id_rsa.pub && chmod 700 /home/$username/.ssh/ && chmod 600 /home/$username/.ssh/authorized_keys"
 
-    if ($?){
+    if ($?) {
 
         spawnBanner;
         Write-Host -NoNewline -ForegroundColor Yellow "[*] "
         Write-Host -NoNewline -ForegroundColor DarkCyan "Successful. Connect with "
         Write-Host -ForegroundColor Green "ssh $username@$hostname -p $port"
     }
-
 }
 
 function main($data) {
@@ -110,7 +110,8 @@ function main($data) {
     if ($data.Count -gt 3) {
         publicKey;
         configHost(checkInput($data));
-    }else{
+    }
+    else {
 
         help;
     }
